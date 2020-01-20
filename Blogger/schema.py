7656dc6768cfb60a0a5f5ebc4ae5252abb80197c
bimbimprasetyoafif <bimo.prasetyoafif@gmail.com>
2020-01-21 00:10:29 +0700

    create post class

--- Blogger/schema.py
+++ Blogger/schema.py
@@ -1,7 +1,10 @@
 import graphene
 import Postingan.schema as sc
 
+class Mutation(sc.Mutation, graphene.ObjectType):
+    pass
+
 class Query(sc.Query, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- Postingan/schema.py
+++ Postingan/schema.py
@@ -6,6 +6,31 @@ class PostinganType(DjangoObjectType):
     class Meta:
         model = Postingan
 
+class CreatePostingan(graphene.Mutation):
+    id = graphene.Int()
+    judul = graphene.String()
+    isi = graphene.String()
+
+    class Arguments:
+        judul = graphene.String()
+        isi = graphene.String()
+    
+    def mutate(self,info,judul,isi):
+        posting = Postingan(
+            judul = judul,
+            isi = isi
+        )
+        posting.save()
+
+        return CreatePostingan(
+            id = posting.id,
+            judul = posting.judul,
+            isi = posting.isi
+        )
+
+class Mutation(object):
+    tambah_postingan = CreatePostingan.Field()
+
 class Query(object):
     semua_postingan = graphene.List(PostinganType)
 
